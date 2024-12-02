a123215a5afd5a022b883fdb4432f0ca5b29c1e9
Itto Kawamitsu <e245714@ie.u-ryukyu.ac.jp>
2024-12-02 10:26:56 +0900

    1st commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245714/Hero.java
@@ -0,0 +1,87 @@
+package jp.ac.uryukyu.ie.e245714;
+
+/**
+ * ヒーロークラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+public class Hero {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+     /* Heroの名前を参照するメソッド */
+    public String getName(){
+        return this.name;
+    }
+    /* HeroのHPを参照するメソッド*/
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+    /*Heroが与えたダメージを参照するメソッド*/
+    public int getAttack(){
+        return this.attack;
+    }
+    /*Heroが死んでいるかどうかを参照するメソッド */
+    public boolean isDead(){
+        return this.dead;
+    }
+    /*Heroの名前を設定するメソッド */
+    public void setName(String _name){
+        this.name = _name;
+    }
+    /*HeroのHPを設定するメソッド */
+    public void setHitPoint(int _hitPoint){
+        this.hitPoint = _hitPoint;
+    }    
+     /*Heroが与えたダメージを設定するメソッド */
+    public void setAttack(int _attack){
+        this.attack = _attack;
+    }
+    /*Heroが死んでいるかどうかを設定するメソッド*/
+    public void setDead(boolean _dead){
+        this.dead = _dead;
+    }
+
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name ヒーロー名
+     * @param maximumHP ヒーローのHP
+     * @param attack ヒーローの攻撃力
+     */
+    public Hero (String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+    
+    /**
+     * Enemyへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param e 攻撃対象
+     */
+    public void attack(Enemy e){
+        int damage = (int)(Math.random() * attack);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
+        e.wounded(damage);
+    }
+    
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+        }
+    }
+}
